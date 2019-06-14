be67a90c310f87c4904eefbd1fbbe3952d994671
Sean Holden <seanholden1@gmail.com>
2019-06-14 15:34:19 +0100

    Added new "cardstack" example as something different

---
+++ cardstack/cardstack.js
@@ -0,0 +1,101 @@
+import TweenMax from "TweenMax";
+import ScrollMagic from "ScrollMagic";
+import "animation.gsap";
+import "debug.addIndicators";
+import { Linear } from "gsap";
+
+const DURATION = {
+  FOREVER: 0
+};
+
+const init = (w, d) => {
+  w.onload = () => main(d);
+};
+
+const defaultElementPosition = () => ({
+  x: 0,
+  y: 0,
+  scale: 1,
+  rotation: 0,
+  ease: Linear.easeNone
+});
+
+const main = d => {
+  const elOne = d.querySelector(".carousel__section--one");
+
+  const c = new ScrollMagic.Controller();
+  scene(c)
+    .from(0)
+    .duration(elOne.clientHeight)
+    .pin(".carousel__section--three");
+  scene(c)
+    .from(0)
+    .duration(elOne.clientHeight)
+    .move(".carousel__section--two")
+    .fromRight();
+  scene(c)
+    .from(elOne.clientHeight)
+    .duration(DURATION.FOREVER)
+    .pin(".carousel__section--two");
+  scene(c)
+    .from(elOne.clientHeight)
+    .duration(elOne.clientHeight)
+    .move(".carousel__section--three")
+    .fromLeft();
+};
+
+const scene = controller => ({
+  from: offset => ({
+    duration: duration => ({
+      pin: el => {
+        controller.addScene(
+          new ScrollMagic.Scene({ duration, offset }).setPin(el)
+        );
+      },
+      move: el => ({
+        fromRight: () => {
+          const scaleBy = 0.5;
+          controller.addScene(
+            new ScrollMagic.Scene({ duration, offset })
+              .setTween(
+                TweenMax.fromTo(
+                  el,
+                  1,
+                  {
+                    x: duration * scaleBy,
+                    y: duration * (1 + scaleBy / 2),
+                    scale: 1 + scaleBy,
+                    rotation: -45
+                  },
+                  defaultElementPosition()
+                )
+              )
+              .setPin(el)
+          );
+        },
+        fromLeft: () => {
+          const scaleBy = 0.5;
+          controller.addScene(
+            new ScrollMagic.Scene({ duration, offset })
+              .setTween(
+                TweenMax.fromTo(
+                  el,
+                  1,
+                  {
+                    x: -duration * scaleBy,
+                    y: duration * (1 + scaleBy / 2),
+                    scale: 1.5,
+                    rotation: 45
+                  },
+                  defaultElementPosition()
+                )
+              )
+              .setPin(el)
+          );
+        }
+      })
+    })
+  })
+});
+
+init(window, document);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './index.js',
-    mode: 'development',
-    output: {
-        filename: 'index.min.js',
-        path: path.resolve(__dirname)
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname),
-        useLocalIp: true,
-        host: '0.0.0.0'
+  entry: {
+    scrollsnaphorizontal: "./scrollsnaphorizontal/scrollsnaphorizontal.js",
+    scrollsnapvertical: "./scrollsnapvertical/scrollsnapvertical.js",
+    cardstack: "./cardstack/cardstack.js"
+  },
+  resolve: {
+    modules: ["node_modules"],
+    alias: {
+      TweenLite: "gsap/src/minified/TweenLite.min.js",
+      TweenMax: "gsap/src/minified/TweenMax.min.js",
+      TimelineLite: "gsap/src/minified/TimelineLite.min.js",
+      TimelineMax: "gsap/src/minified/TimelineMax.min.js",
+      ScrollMagic: "scrollmagic/scrollmagic/minified/ScrollMagic.min.js",
+      "animation.gsap":
+        "scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js",
+      "debug.addIndicators":
+        "scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js"
     }
+  },
+  mode: "development",
+  output: {
+    filename: "[name]/[name].min.js",
+    path: path.resolve(__dirname)
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname),
+    useLocalIp: true,
+    host: "0.0.0.0"
+  }
 };
