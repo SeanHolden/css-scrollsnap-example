bfae8968a9b291b763eed55c624253d416c00127
Sean Holden <seanholden1@gmail.com>
2019-07-21 14:42:55 +0100

    Now have elements flying in from right and left

--- cardstack/cardstack.js
+++ cardstack/cardstack.js
@@ -8,8 +8,8 @@ const DURATION = {
   FOREVER: 0
 };
 
-const init = (w, d) => {
-  w.onload = () => main(d);
+const init = () => {
+  window.onload = () => main();
 };
 
 const defaultElementPosition = () => ({
@@ -20,14 +20,23 @@ const defaultElementPosition = () => ({
   ease: Linear.easeNone
 });
 
-const main = d => {
-  const elOne = d.querySelector(".carousel__section--one");
+const main = () => {
+  awesomeScroll([
+    ".carousel__section--one",
+    ".carousel__section--two",
+    ".carousel__section--three"
+  ]);
+};
+
+const awesomeScroll = (selectors) => {
+  const elOne = window.document.querySelector(".carousel__section--one");
 
   const c = new ScrollMagic.Controller();
-  // scene(c)
-  //   .from(0)
-  //   .duration(elOne.clientHeight)
-  //   .pin(".carousel__section--three");
+  scene(c)
+    .from(0)
+    .duration(elOne.clientHeight)
+    .pin(".carousel__section--three");
+
   scene(c)
     .from(0)
     .duration(elOne.clientHeight)
@@ -37,89 +46,105 @@ const main = d => {
     .from(elOne.clientHeight)
     .duration(DURATION.FOREVER)
     .pin(".carousel__section--two");
-  // scene(c)
-  //   .from(elOne.clientHeight)
-  //   .duration(elOne.clientHeight)
-  //   .move(".carousel__section--three")
-  //   .fromLeft();
-  // scene(c)
-  //   .from(elOne.clientHeight * 2)
-  //   .duration(DURATION.FOREVER)
-  //   .pin(".carousel__section--three");
+
+  scene(c)
+    .from(elOne.clientHeight)
+    .duration(elOne.clientHeight)
+    .move(".carousel__section--three")
+    .fromLeft();
+  scene(c)
+    .from(elOne.clientHeight)
+    .duration(DURATION.FOREVER)
+    .pin(".carousel__section--three");
 };
 
-const scene = controller => ({
-  from: offset => ({
-    duration: duration => ({
-      pin: el => {
-        controller.addScene(
-          new ScrollMagic.Scene({ duration, offset }).setPin(el)
-        );
-      },
-      move: el => ({
-        fromRight: () => {
-          const angleDeg = 30;
-          const scale = 2;
+const scene = controller => {
+  const angleDeg = 50;
+  const scale = 2;
+  const angle = (angleDeg * Math.PI) / 180;
+
+  return {
+    from: offset => ({
+      duration: duration => ({
+        pin: el => {
+          controller.addScene(
+            new ScrollMagic.Scene({ duration, offset }).setPin(el)
+          );
+        },
+        move: el => {
           const element = document.querySelector(el);
-          const angle = angleDeg * Math.PI / 180;
           const originalWidth = element.clientWidth * scale;
           const originalHeight = element.clientHeight * scale;
-          const widthAfterRotation = ((originalWidth * Math.cos(angle)) + (originalHeight * Math.sin(angle)));
-          const heightAfterRotation = ((originalWidth * Math.sin(angle)) + (originalHeight * Math.cos(angle)));
+          const widthAfterRotation =
+            originalWidth * Math.cos(angle) + originalHeight * Math.sin(angle);
+          const heightAfterRotation =
+            originalWidth * Math.sin(angle) + originalHeight * Math.cos(angle);
 
           // Work out x position.
-          const xDifferenceAfterRotation = (widthAfterRotation - originalWidth);
-          const xDifferenceBetweenScreenAndRect = originalWidth - window.innerWidth;
-          const correctXPosition = (originalWidth + ((xDifferenceAfterRotation/2))) - (xDifferenceBetweenScreenAndRect/2);
+          const xDifferenceAfterRotation = widthAfterRotation - originalWidth;
+          const xDifferenceBetweenScreenAndRect =
+            originalWidth - window.innerWidth;
+          const correctXPosition =
+            originalWidth +
+            xDifferenceAfterRotation / 2 -
+            xDifferenceBetweenScreenAndRect / 2;
 
           // Work out y position.
           const yDifferenceAfterRotation = heightAfterRotation - originalHeight;
-          const topOfRotatedShapeToLeftPoint = originalWidth * Math.sin(angle); // right triangle vertical leg given angle & hypotenuse
+          const topOfRotatedShapeToLeftPoint = originalWidth * Math.sin(angle); // right triangle vertical leg, given angle & hypotenuse
+          const pointPositionFromBottom =
+            topOfRotatedShapeToLeftPoint - window.innerHeight * scale;
+          const yDifferenceBetweenScreenAndRect =
+            originalHeight - window.innerHeight;
+          const correctYPosition =
+            yDifferenceAfterRotation / 2 -
+            pointPositionFromBottom -
+            yDifferenceBetweenScreenAndRect / 2;
 
-          const pointPositionFromBottom = topOfRotatedShapeToLeftPoint - (window.innerHeight * scale);
-          const yDifferenceBetweenScreenAndRect = originalHeight - window.innerHeight;
-          const correctYPosition = ((yDifferenceAfterRotation/2) - pointPositionFromBottom) - (yDifferenceBetweenScreenAndRect/2);
-
-            controller.addScene(
-            new ScrollMagic.Scene({ duration, offset })
-              .setTween(
-                TweenMax.fromTo(
-                  el,
-                  1,
-                  {
-                    x: correctXPosition - (window.innerWidth/2),
-                    y: correctYPosition - (window.innerHeight/2),
-                    scale,
-                    rotation: -angleDeg
-                  },
-                  defaultElementPosition()
-                )
-              )
-              .setPin(el)
-          );
-        },
-        fromLeft: () => {
-          controller.addScene(
-            new ScrollMagic.Scene({ duration, offset })
-              .setTween(
-                TweenMax.fromTo(
-                  el,
-                  1,
-                  {
-                    x: 0,
-                    y: duration * 1.25,
-                    scale: 1.5,
-                    rotation: 45
-                  },
-                  defaultElementPosition()
-                )
-              )
-              .setPin(el)
-          );
+          return {
+            fromRight: () => {
+              controller.addScene(
+                new ScrollMagic.Scene({ duration, offset })
+                  .setTween(
+                    TweenMax.fromTo(
+                      el,
+                      1,
+                      {
+                        x: correctXPosition,
+                        y: correctYPosition,
+                        scale,
+                        rotation: -angleDeg
+                      },
+                      defaultElementPosition()
+                    )
+                  )
+                  .setPin(el)
+              );
+            },
+            fromLeft: () => {
+              controller.addScene(
+                new ScrollMagic.Scene({ duration, offset })
+                  .setTween(
+                    TweenMax.fromTo(
+                      el,
+                      1,
+                      {
+                        x: -correctXPosition,
+                        y: correctYPosition,
+                        scale,
+                        rotation: angleDeg
+                      },
+                      defaultElementPosition()
+                    )
+                  )
+                  .setPin(el)
+              );
+            }
+          };
         }
       })
     })
-  })
-});
+  };
+};
 
-init(window, document);
+init();
